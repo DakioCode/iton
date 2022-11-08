0419cda0907cba461a15fb9db1caea2a55ba5626
DankBroom <91276577+DankBroom@users.noreply.github.com>
2022-11-08 18:34:46 +0100

    rename index.php

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
 </head>
 
 <body>
-    <img src="https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png" alt="Logo d'Iton"
-        id="logo">
+    <img src="https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png" alt="Logo d'Iton" id="logo">
     <div id="content">
         <form action="" method="post">
             <input type="email" placeholder="Adresse-mail">
