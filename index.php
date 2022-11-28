b804c566389aaf8c7c0f36acfdc9f38fb830ba1a
BlackCoder14 <snn.67@hotmail.com>
2022-11-28 16:53:51 +0100

    Nettoyage + ajout du logo (page connexion)

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ if (isset($_POST["username"])) {
 ?>
 
 <!DOCTYPE html>
-<!-- <img src="https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png" alt="Logo d'Iton" id="logo"> -->
 <html>
 
 <head>
@@ -28,6 +27,8 @@ if (isset($_POST["username"])) {
 
 <body>
     <div id="container">
+        <!-- <img src="/assets/logo_ban.png" alt="Logo d'Iton" id="logo"> -->
+
         <form action="" method="POST">
             <h1>Connexion</h1>
 
@@ -37,7 +38,7 @@ if (isset($_POST["username"])) {
             <label><b>Mot de passe</b></label>
             <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-            <input type="submit" id='submit' value='Connexion'>
+            <button type="submit">Connexion</button>
             <p align="center">C'est la première fois ? <a href="/inscription.php">S'inscrire</a>.</p>
         </form>
     </div>
--- src/verifications/is_connected.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function isConnectedVerification(): bool
-{
-    if (!isset($_SESSION["username"])) return false;
-
-    return true;
-}
